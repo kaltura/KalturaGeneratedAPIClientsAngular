411979b651d25d3a09692710eb6f9cedfef24e95
Kaltura CSI <community@kaltura.com>
2018-02-08 04:12:01 -0500

    New clientlib ver

--- src/api/types/KalturaLanguageCode.ts
+++ src/api/types/KalturaLanguageCode.ts
@@ -164,8 +164,6 @@ export class KalturaLanguageCode extends KalturaObjectBase {
 	static yi = new KalturaLanguageCode('yi');
 	static yo = new KalturaLanguageCode('yo');
 	static zh = new KalturaLanguageCode('zh');
-	static zhHans = new KalturaLanguageCode('zh_s');
-	static zhHant = new KalturaLanguageCode('zh_t');
 	static zu = new KalturaLanguageCode('zu');
 }
 KalturaTypesFactory.registerType('KalturaLanguageCode',KalturaLanguageCode);
