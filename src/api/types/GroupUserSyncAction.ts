c5780d151211f04037d1d90e21346950368162e0
Kaltura CSI <community@kaltura.com>
2018-05-22 03:26:00 -0400

    New clientlib ver

--- src/api/types/GroupUserSyncAction.ts
+++ src/api/types/GroupUserSyncAction.ts
@@ -7,6 +7,8 @@ import { KalturaRequest, KalturaRequestArgs } from '../kaltura-request';
 export interface GroupUserSyncActionArgs  extends KalturaRequestArgs {
     userId : string;
 	groupIds : string;
+	removeFromExistingGroups? : boolean;
+	createNewGroups? : boolean;
 }
 
 /**
@@ -23,10 +25,14 @@ export class GroupUserSyncAction extends KalturaRequest<KalturaBulkUpload> {
 
     userId : string;
 	groupIds : string;
+	removeFromExistingGroups : boolean;
+	createNewGroups : boolean;
 
     constructor(data : GroupUserSyncActionArgs)
     {
         super(data, {responseType : 'o', responseSubType : 'KalturaBulkUpload', responseConstructor : KalturaBulkUpload  });
+        if (typeof this.removeFromExistingGroups === 'undefined') this.removeFromExistingGroups = true;
+		if (typeof this.createNewGroups === 'undefined') this.createNewGroups = true;
     }
 
     protected _getMetadata() : KalturaObjectMetadata
@@ -38,7 +44,9 @@ export class GroupUserSyncAction extends KalturaRequest<KalturaBulkUpload> {
                 service : { type : 'c', default : 'groupuser' },
 				action : { type : 'c', default : 'sync' },
 				userId : { type : 's' },
-				groupIds : { type : 's' }
+				groupIds : { type : 's' },
+				removeFromExistingGroups : { type : 'b' },
+				createNewGroups : { type : 'b' }
             }
         );
         return result;
