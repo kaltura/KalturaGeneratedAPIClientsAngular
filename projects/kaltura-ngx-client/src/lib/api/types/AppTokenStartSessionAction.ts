d11b098354d6f721221134718a279d4fe5a2d756
Kaltura CSI <community@kaltura.com>
2018-08-30 04:18:01 -0400

    New clientlib ver

--- projects/kaltura-ngx-client/src/lib/api/types/AppTokenStartSessionAction.ts
+++ projects/kaltura-ngx-client/src/lib/api/types/AppTokenStartSessionAction.ts
@@ -11,6 +11,7 @@ export interface AppTokenStartSessionActionArgs  extends KalturaRequestArgs {
 	userId? : string;
 	type? : KalturaSessionType;
 	expiry? : number;
+	sessionPrivileges? : string;
 }
 
 /**
@@ -28,6 +29,7 @@ export class AppTokenStartSessionAction extends KalturaRequest<KalturaSessionInf
 	userId : string;
 	type : KalturaSessionType;
 	expiry : number;
+	sessionPrivileges : string;
 
     constructor(data : AppTokenStartSessionActionArgs)
     {
@@ -46,7 +48,8 @@ export class AppTokenStartSessionAction extends KalturaRequest<KalturaSessionInf
 				tokenHash : { type : 's' },
 				userId : { type : 's' },
 				type : { type : 'en', subTypeConstructor : KalturaSessionType, subType : 'KalturaSessionType' },
-				expiry : { type : 'n' }
+				expiry : { type : 'n' },
+				sessionPrivileges : { type : 's' }
             }
         );
         return result;
--- projects/kaltura-ngx-client/src/lib/environment.ts
+++ projects/kaltura-ngx-client/src/lib/environment.ts
@@ -9,7 +9,7 @@ export interface Environment {
 
 export const environment: Environment = {
     request: {
-        apiVersion: '14.5.0'
+        apiVersion: '14.6.0'
     },
     response: {
         nestedResponse: false
