5d815149e9831e69459d2e676baa6c08dc58b887
Kaltura CSI <community@kaltura.com>
2018-11-26 01:43:24 -0500

    New clientlib ver

--- projects/kaltura-ngx-client/src/lib/api/types/KalturaReportType.ts
+++ projects/kaltura-ngx-client/src/lib/api/types/KalturaReportType.ts
@@ -3,7 +3,7 @@
 export enum KalturaReportType {
     applications = '16',
 	browsers = '23',
-	browsersFamiiles = '33',
+	browsersFamilies = '33',
 	cities = '30',
 	contentContributions = '7',
 	contentDropoff = '2',
