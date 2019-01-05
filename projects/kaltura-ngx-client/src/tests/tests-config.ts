6633b01b94f0ab4a671dcfa3f37a33a9ca08c9a3
Kaltura CSI <community@kaltura.com>
2019-01-05 02:44:16 -0500

    New clientlib ver

--- projects/kaltura-ngx-client/src/tests/tests-config.ts
+++ projects/kaltura-ngx-client/src/tests/tests-config.ts
@@ -2,6 +2,6 @@ export const TestsConfig = {
   partnerId: "@YOUR_PARTNER_ID@",
   userName: "testUser",
   adminSecret: "@YOUR_ADMIN_SECRET@",
-  endpointUrl: "https://www.kaltura.com",
+  endpointUrl: "@SERVICE_URL@",
   clientTag: "ngx-tests"
 };
