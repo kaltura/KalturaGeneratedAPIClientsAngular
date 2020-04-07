f0e13f4f0623c3cc2211a3d4c92601aedb2ed051
Kaltura CSI <community@kaltura.com>
2020-04-06 21:03:08 -0400

    New clientlib ver

--- projects/kaltura-ngx-client/src/lib/api/types/KalturaVendorServiceTurnAroundTime.ts
+++ projects/kaltura-ngx-client/src/lib/api/types/KalturaVendorServiceTurnAroundTime.ts
@@ -7,11 +7,13 @@ export enum KalturaVendorServiceTurnAroundTime {
 	fortyEightHours=172800,
 	fourDays=345600,
 	immediate=0,
+	oneBusinessDay=129600,
 	sixHours=21600,
 	tenDays=864000,
 	thirtyMinutes=1800,
 	threeHours=10800,
 	twelveHours=43200,
 	twentyFourHours=86400,
+	twoBusinessDays=216000,
 	twoHours=7200
 }
