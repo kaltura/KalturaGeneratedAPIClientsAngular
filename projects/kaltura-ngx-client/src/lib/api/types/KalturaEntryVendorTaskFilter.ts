fb8eb2c493fd392077f1b50b4bc6d0df6862d100
Kaltura CSI <community@kaltura.com>
2021-08-31 20:03:37 -0400

    New clientlib ver

--- projects/kaltura-ngx-client/src/lib/api/types/KalturaEntryVendorTaskFilter.ts
+++ projects/kaltura-ngx-client/src/lib/api/types/KalturaEntryVendorTaskFilter.ts
@@ -4,16 +4,12 @@ import { KalturaEntryVendorTaskBaseFilter, KalturaEntryVendorTaskBaseFilterArgs 
 
 export interface KalturaEntryVendorTaskFilterArgs  extends KalturaEntryVendorTaskBaseFilterArgs {
     freeText? : string;
-	expectedFinishTimeGreaterThanOrEqual? : Date;
-	expectedFinishTimeLessThanOrEqual? : Date;
 }
 
 
 export class KalturaEntryVendorTaskFilter extends KalturaEntryVendorTaskBaseFilter {
 
     freeText : string;
-	expectedFinishTimeGreaterThanOrEqual : Date;
-	expectedFinishTimeLessThanOrEqual : Date;
 
     constructor(data? : KalturaEntryVendorTaskFilterArgs)
     {
@@ -27,9 +23,7 @@ export class KalturaEntryVendorTaskFilter extends KalturaEntryVendorTaskBaseFilt
             result.properties,
             {
                 objectType : { type : 'c', default : 'KalturaEntryVendorTaskFilter' },
-				freeText : { type : 's' },
-				expectedFinishTimeGreaterThanOrEqual : { type : 'd' },
-				expectedFinishTimeLessThanOrEqual : { type : 'd' }
+				freeText : { type : 's' }
             }
         );
         return result;
