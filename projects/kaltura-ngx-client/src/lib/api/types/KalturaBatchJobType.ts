b4c60e08d549d54ccfe6f589b87a1983113d8f64
Kaltura CSI <community@kaltura.com>
2022-01-19 20:04:19 -0500

    New clientlib ver

--- projects/kaltura-ngx-client/src/lib/api/types/KalturaBatchJobType.ts
+++ projects/kaltura-ngx-client/src/lib/api/types/KalturaBatchJobType.ts
@@ -32,6 +32,7 @@ export enum KalturaBatchJobType {
 	dropFolderWatcher = 'dropFolder.DropFolderWatcher',
 	dvdcreator = '5',
 	emailIngestion = '26',
+	entryVendorTaskCsv = 'reach.EntryVendorTasksCsv',
 	eventNotificationHandler = 'eventNotification.EventNotificationHandler',
 	exportCsv = '49',
 	extractMedia = '14',
@@ -68,8 +69,10 @@ export enum KalturaBatchJobType {
 	storagePeriodicPurge = '54',
 	storageUpdate = '52',
 	syncCategoryPrivacyContext = '39',
+	syncReachCreditTask = 'reach.SyncReachCreditTask',
 	tagResolve = 'tagSearch.TagResolve',
 	usersCsv = '46',
 	validateLiveMediaServers = '38',
-	virusScan = 'virusScan.VirusScan'
+	virusScan = 'virusScan.VirusScan',
+	widevineRepositorySync = 'widevine.WidevineRepositorySync'
 }
