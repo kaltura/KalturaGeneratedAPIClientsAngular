4964ab0abde1e0241107ba7210864ddcc7348467
Kaltura CSI <community@kaltura.com>
2022-08-22 20:07:15 -0400

    New clientlib ver

--- projects/kaltura-ngx-client/src/lib/api/types/KalturaUiConfAdminOrderBy.ts
+++ projects/kaltura-ngx-client/src/lib/api/types/KalturaUiConfAdminOrderBy.ts
@@ -3,6 +3,10 @@
 export enum KalturaUiConfAdminOrderBy {
     createdAtAsc = '+createdAt',
 	createdAtDesc = '-createdAt',
+	idAsc = '+id',
+	idDesc = '-id',
+	nameAsc = '+name',
+	nameDesc = '-name',
 	updatedAtAsc = '+updatedAt',
 	updatedAtDesc = '-updatedAt'
 }
--- projects/kaltura-ngx-client/src/lib/api/types/KalturaUiConfOrderBy.ts
+++ projects/kaltura-ngx-client/src/lib/api/types/KalturaUiConfOrderBy.ts
@@ -3,6 +3,10 @@
 export enum KalturaUiConfOrderBy {
     createdAtAsc = '+createdAt',
 	createdAtDesc = '-createdAt',
+	idAsc = '+id',
+	idDesc = '-id',
+	nameAsc = '+name',
+	nameDesc = '-name',
 	updatedAtAsc = '+updatedAt',
 	updatedAtDesc = '-updatedAt'
 }
