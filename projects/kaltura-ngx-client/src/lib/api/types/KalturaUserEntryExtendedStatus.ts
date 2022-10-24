7a10bde655220b704856b6b31982fbfaac74c144
Kaltura CSI <community@kaltura.com>
2022-10-24 08:32:25 -0400

    New clientlib ver

--- projects/kaltura-ngx-client/src/lib/api/types/KalturaUserEntryExtendedStatus.ts
+++ projects/kaltura-ngx-client/src/lib/api/types/KalturaUserEntryExtendedStatus.ts
@@ -3,5 +3,7 @@
 export enum KalturaUserEntryExtendedStatus {
     playbackComplete = 'viewHistory.PLAYBACK_COMPLETE',
 	playbackStarted = 'viewHistory.PLAYBACK_STARTED',
+	syncStatusError = 'quiz.SYNC_STATUS_ERROR',
+	syncStatusSuccess = 'quiz.SYNC_STATUS_SUCCESS',
 	viewed = 'viewHistory.VIEWED'
 }
